0bd411538986a12a8d2861fd98cdbc08eb4c6de4
qiuyuzhao <1453304741@qq.com>
2022-06-10 15:55:31 +0800

    feature_dim为64，num_layers=1

--- modules/audio2ky_faceformer.py
+++ modules/audio2ky_faceformer.py
@@ -101,7 +101,7 @@ class Faceformer(nn.Module):
         # temporal bias
         self.biased_mask = init_biased_mask(n_head=4, max_seq_len=600, period=args.period)
         decoder_layer = nn.TransformerDecoderLayer(d_model=args.feature_dim, nhead=4, dim_feedforward=2*args.feature_dim, batch_first=True)
-        self.transformer_decoder = nn.TransformerDecoder(decoder_layer, num_layers=4)
+        self.transformer_decoder = nn.TransformerDecoder(decoder_layer, num_layers=1)
         # motion decoder
         self.vertice_map_r = nn.Linear(args.feature_dim, args.vertice_dim)
         self.device = device
